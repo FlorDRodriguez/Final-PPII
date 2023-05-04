b0d50cfa3d7c0b319838dac49808cfac71c5f5e9
Marcos <cacoxg96@gmail.com>
2023-05-04 18:29:23 -0300

    DB actualizada

--- database/db.sql
+++ database/db.sql
@@ -87,27 +87,26 @@ DESCRIBE libros;
 
 
 CREATE TABLE alumnos (
-    legajo INT(11) NOT NULL,
     dni INT(11) NOT NULL,
     nombre VARCHAR(45) NOT NULL,
     apellido VARCHAR(45) NOT NULL,
     direccion VARCHAR(45) NOT NULL,
     telefono VARCHAR(45),
     contraseña VARCHAR(45) NOT NULL,
-    PRIMARY KEY (legajo)
+    PRIMARY KEY (dni)
 );
 
 DESCRIBE alumnos;
 
 
 CREATE TABLE profesores (
-    nroBono INT(11) NOT NULL,
     dni INT(11) NOT NULL,
     nombre VARCHAR(45) NOT NULL,
     apellido VARCHAR(45) NOT NULL,
     direccion VARCHAR(45) NOT NULL,
     telefono VARCHAR(45),
-    PRIMARY KEY (nroBono)
+    contraseña VARCHAR(45) NOT NULL,
+    PRIMARY KEY (dni)
 );
 
 DESCRIBE profesores;
@@ -124,7 +123,7 @@ CREATE TABLE cursos (
     tipo INT(1) NOT NULL,
     CONSTRAINT fk_tipoCurso FOREIGN KEY(tipo) REFERENCES tipoCursos(idTipoCurso),
     profesor INT(11) NOT NULL, 
-    CONSTRAINT fk_profesor FOREIGN KEY(profesor) REFERENCES profesores(nroBono),
+    CONSTRAINT fk_profesor FOREIGN KEY(profesor) REFERENCES profesores(dni),
     libro INT(11),
     CONSTRAINT fk_libro FOREIGN KEY(libro) REFERENCES libros(idLibro),
     PRIMARY KEY (idCurso)
@@ -141,7 +140,7 @@ CREATE TABLE anuncios (
     curso INT(11) NOT NULL,
     CONSTRAINT fk_curso FOREIGN KEY(curso) REFERENCES cursos(idCurso),
     profesor INT(11) NOT NULL,
-    CONSTRAINT fk_profesor_anuncio FOREIGN KEY(profesor) REFERENCES profesores(nroBono),
+    CONSTRAINT fk_profesor_anuncio FOREIGN KEY(profesor) REFERENCES profesores(dni),
     tipo INT(1) NOT NULL,
     CONSTRAINT fk_tipoAnuncio FOREIGN KEY(tipo) REFERENCES tipoAnuncios(idTipoAnuncio),
     PRIMARY KEY (idAnuncio)
@@ -153,7 +152,7 @@ DESCRIBE anuncios;
 CREATE TABLE asistenciaCursos (
     idAsistenciaCurso INT(11) NOT NULL AUTO_INCREMENT,
     alumno INT(11) NOT NULL,
-    CONSTRAINT fk_alumno FOREIGN KEY(alumno) REFERENCES alumnos(legajo),
+    CONSTRAINT fk_alumno FOREIGN KEY(alumno) REFERENCES alumnos(dni),
     curso INT(11) NOT NULL,
     CONSTRAINT fk_curso_asistencia FOREIGN KEY(curso) REFERENCES cursos(idCurso),
     presentes INT(11) NOT NULL,
@@ -169,7 +168,7 @@ DESCRIBE asistenciaCursos;
 CREATE TABLE listaAlumnos (
     idListaAlumnos INT(11) NOT NULL AUTO_INCREMENT,
     alumno INT(11) NOT NULL,
-    CONSTRAINT fk_alumno_lista FOREIGN KEY(alumno) REFERENCES alumnos(legajo),
+    CONSTRAINT fk_alumno_lista FOREIGN KEY(alumno) REFERENCES alumnos(dni),
     curso INT(11) NOT NULL,
     CONSTRAINT fk_curso_lista FOREIGN KEY(curso) REFERENCES cursos(idCurso),
     PRIMARY KEY (idListaAlumnos)
@@ -187,9 +186,9 @@ CREATE TABLE clases (
     unidadesLibro INT(11) NOT NULL,
     CONSTRAINT fk_unidadesLibro FOREIGN KEY(unidadesLibro) REFERENCES libros(idLibro),
     presentes INT(11) NOT NULL,
-    CONSTRAINT fk_presentes FOREIGN KEY(presentes) REFERENCES alumnos(legajo),
+    CONSTRAINT fk_presentes FOREIGN KEY(presentes) REFERENCES alumnos(dni),
     ausentes INT(11) NOT NULL,
-    CONSTRAINT fk_ausentes FOREIGN KEY(ausentes) REFERENCES alumnos(legajo),
+    CONSTRAINT fk_ausentes FOREIGN KEY(ausentes) REFERENCES alumnos(dni),
     PRIMARY KEY (idClase)
 
 );
