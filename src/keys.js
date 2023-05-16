cc07fc2cf0c83d0331ac1d78ddff78e9a4f72556
Marcos <cacoxg96@gmail.com>
2023-05-16 03:03:32 -0300

    login arreglado

--- src/keys.js
+++ src/keys.js
@@ -3,7 +3,7 @@ module.exports = {
     database: { //objeto de config de la db
         host: 'localhost',//donde esta alojada
         user: 'root',//a traves de qué usuario me voy a conectar
-        password: '',
+        password: 'root',
         database: 'db_attendance'
     }
 
